40477e7be287e4785bbbfd61ef6b86d67228c3e3
Armando López <mundroe@gmail.com>
2021-02-03 19:35:43 -0600

    Commit incluyendo nuevas secciones del Home, nueva página de "Nosotros y Adhesivos"

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 import $ from 'jquery';
 import bootstrapModal from '../node_modules/bootstrap/js/src/modal';
+import bootstrapCarousel from '../node_modules/bootstrap/js/src/carousel';
 
 $(document).ready(() => {
 	console.log("It's working! It's working!!");
 });
 
-export { bootstrapModal };
+export { bootstrapModal, bootstrapCarousel };
