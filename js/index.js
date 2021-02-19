e9066deac4158760f0941272e9a53678a4ef24b1
Uhen <mankeyearth@gmail.com>
2021-02-19 16:34:50 -0600

    Added bootstrap dependencies to index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 import $ from 'jquery';
+import Popper from 'popper';
 import bootstrapModal from '../node_modules/bootstrap/js/src/modal';
 import bootstrapCarousel from '../node_modules/bootstrap/js/src/carousel';
-
+import bootstrapDropdown from '../node_modules/bootstrap/js/src/dropdown';
+import bootstrapCollapse from '../node_modules/bootstrap/js/src/collapse';
 $(document).ready(() => {
-	console.log("It's working! It's working!!");
+	$('.needs-validation').each(function() {
+		const form = $(this);
+		form.submit(function(event) {
+			if (form[0].checkValidity() === false) {
+				event.preventDefault()
+				event.stopPropagation()
+			}
+			form.addClass('was-validated')
+		})
+	})
 });
-
-export { bootstrapModal, bootstrapCarousel };
+export { bootstrapModal, bootstrapCarousel, bootstrapDropdown };
